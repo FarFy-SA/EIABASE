a4baf285159db29385970c0b4badca44dd6ed824
FarFy-SA <jcpd.javi@gmail.com>
2025-05-14 20:56:12 -0600

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+# Configuración de página
+st.set_page_config(page_title="Clase de Programación", layout="wide")
+
+
+with st.sidebar:
+    st.markdown("Clases")
+    st.button("Ejemplo clase 1")
+    st.button("Ejemplo clase 2")
+    st.button("Ejemplo clase 3")
+    st.markdown("---")
+    st.markdown("Persona Ejemplo")
+
+
+st.markdown(
+    "<h1 style='text-align: center;'>Bienvenido a tu clase de programación</h1>",
+    unsafe_allow_html=True
+)
+
+# Área del "chat" simulado
+st.markdown("## ")
+pregunta = st.text_input("Escribe tu pregunta:", placeholder="Quiero saber sobre...")
+
+if pregunta:
+    st.success(f"Respuesta simulada a: **{pregunta}**")
